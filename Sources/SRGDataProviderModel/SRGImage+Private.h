694a29e519b9ad4ddf29b72a32f7a823dfa91bfb
Samuel Défago <defagos@gmail.com>
2022-04-20 16:05:57 +0200

    Expose image variant information

--- Sources/SRGDataProviderModel/SRGImage+Private.h
+++ Sources/SRGDataProviderModel/SRGImage+Private.h
@@ -20,11 +20,6 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @property (nonatomic, readonly) NSURL *URL;
 
-/**
- *  The variant which the image must be scaled for.
- */
-@property (nonatomic, readonly) SRGImageVariant variant;
-
 @end
 
 NS_ASSUME_NONNULL_END
--- Sources/SRGDataProviderModel/include/SRGImage.h
+++ Sources/SRGDataProviderModel/include/SRGImage.h
@@ -13,6 +13,11 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface SRGImage : NSObject
 
+/**
+ *  The image variant.
+ */
+@property (nonatomic, readonly) SRGImageVariant variant;
+
 @end
 
 NS_ASSUME_NONNULL_END
