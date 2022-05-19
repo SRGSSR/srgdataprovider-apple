0ce5471eac3a5fc8f432a518774aec6e6f07820b
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2022-05-19 15:24:42 +0200

    Update UT with new stream

--- Tests/SRGDataProviderNetworkTests/ChapterTestCase.m
+++ Tests/SRGDataProviderNetworkTests/ChapterTestCase.m
@@ -150,11 +150,11 @@ static NSURL *ServiceTestURL(void)
     XCTestExpectation *expectation = [self expectationWithDescription:@"Ready to play"];
     
     SRGDataProvider *dataProvider = [[SRGDataProvider alloc] initWithServiceURL:ServiceTestURL()];
-    [[dataProvider mediaCompositionForURN:@"urn:rts:video:1967124" standalone:NO withCompletionBlock:^(SRGMediaComposition * _Nullable mediaComposition, NSHTTPURLResponse * _Nullable HTTPResponse, NSError * _Nullable error) {
+    [[dataProvider mediaCompositionForURN:@"urn:rts:video:10623665" standalone:NO withCompletionBlock:^(SRGMediaComposition * _Nullable mediaComposition, NSHTTPURLResponse * _Nullable HTTPResponse, NSError * _Nullable error) {
         SRGChapter *mainChapter = mediaComposition.mainChapter;
-        XCTAssertEqual(mainChapter.resources.count, 2);
-        XCTAssertEqual(mainChapter.playableResources.count, 2);
-        XCTAssertEqual([mainChapter resourcesForStreamingMethod:SRGStreamingMethodHLS].count, 2);
+        XCTAssertEqual(mainChapter.resources.count, 1);
+        XCTAssertEqual(mainChapter.playableResources.count, 1);
+        XCTAssertEqual([mainChapter resourcesForStreamingMethod:SRGStreamingMethodHLS].count, 1);
         XCTAssertEqual([mainChapter resourcesForStreamingMethod:SRGStreamingMethodHDS].count, 0);
         XCTAssertEqual(mainChapter.recommendedStreamingMethod, SRGStreamingMethodHLS);
         XCTAssertEqual(mainChapter.recommendedSubtitleFormat, SRGSubtitleFormatNone);
